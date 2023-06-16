440002dd600081bac6892761abbf644e6394e118
GUNUPURU GUNASHEKAR SAI <113043690+gunasekharsai@users.noreply.github.com>
2023-06-16 16:08:57 +0530

    casual changes

--- client/src/components/header/Navbaar.js
+++ client/src/components/header/Navbaar.js
@@ -1,16 +1,35 @@
 import React from 'react'
 import "./navbar.css"
+import SearchIcon from '@mui/icons-material/Search';
+import ShoppingCartSharpIcon from '@mui/icons-material/ShoppingCartSharp';
+import Avatar from '@mui/material/Avatar';
+import Badge from '@mui/material/Badge';
 const navbaar = () => {
   return (
     <header>
       <nav>
         <div className='left'>
           <div className='navlogo'>
-             <img src = "amazon_logo.png" alt='amazon log' />
+           <img src="./logo.jpg" alt="logo" />
           </div>
-      
+          <div className='nav_searchbaar' >
+            <input type='text' name='' id='' />
+            <div className='search_icon'>
+                 <SearchIcon id = "search" />
+              </div>        
+           </div>
         </div>
         <div className='right'>
+          <div className='nav_btn'>
+            <a href=''>Signin</a>
+          </div>
+          <div className='cart_btn'>
+           <Badge badgeContent={4} color="primary">
+               <ShoppingCartSharpIcon id=" shopping cart" />
+           </Badge>
+              <p>cart</p>
+          </div>
+          <Avatar className='avtar' />
 
         </div>
       </nav>
